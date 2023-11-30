d972694776831b069203ee3fbf18403ca8e8f9d3
CollinAvidano <collin.avidano@gmail.com>
2023-11-29 22:11:43 -0500

    IT FINALLY WORKS WITH GPU

--- base.nix
+++ base.nix
@@ -9,15 +9,39 @@
     { device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
     };
-  boot = {
-    kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-    loader = {
-      generic-extlinux-compatible.enable = lib.mkDefault true;
-      grub.enable = lib.mkDefault false;
-    };
+
+nixpkgs.overlays = [
+  (final: super: {
+    makeModulesClosure = x:
+      super.makeModulesClosure (x // { allowMissing = true; });
+  })
+];
+
+
+
+
+boot = {
+  kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
+  #initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+  loader = {
+    generic-extlinux-compatible.enable = lib.mkDefault true;
+    grub.enable = lib.mkDefault false;
   };
+};
+
+
   nix.settings = {
     experimental-features = lib.mkDefault "nix-command flakes";
     trusted-users = [ "root" "@wheel" ];
   };
+
+hardware = {
+    #raspberry-pi."4".apply-overlays-dtmerge.enable = true;
+    raspberry-pi."4".fkms-3d.enable = true;
+    #raspberry-pi."4".audio.enable = true;
+    #pulseaudio.enable = true;
+    enableRedistributableFirmware = true;
+};
+#sound.enable = true;
+console.enable = false;
 }
